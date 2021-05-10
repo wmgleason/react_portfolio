dd3c957a95bb5987b843a2b804fab4fd457548b0
Wanda Gleason <oglissane@gmail.com>
2021-05-09 22:53:46 -0400

    Added contact component.

---
+++ src/components/contact.js
@@ -0,0 +1,43 @@
+import React, { Component } from "react";
+import { Grid, Cell, List, ListItem, ListItemContent } from "react-mdl";
+
+class Contact extends Component {
+  render() {
+    return (
+      <div className="contact-body">
+        <Grid className="contact-grid">
+          <Cell col={6}>
+            <h2>Wanda Gleason</h2>
+            <img
+              src="./../public/img/profile.jpg"
+              alt="avatar"
+              style={{ height: "250px" }}
+            />
+            <p style={{ width: "75%", margin: "auto", paddingTop: "1em" }}>
+              Let's work together.
+            </p>
+          </Cell>
+          <Cell col={6}>
+            <h2>Contact Me:</h2>
+            <hr />
+
+            <div className="contact-list">
+              <List>
+                <ListItem>
+                  <ListItemContent
+                    style={{ fontSize: "30px", fontFamily: "Anton" }}
+                  >
+                    <i className="fa fa-envelope" aria-hidden="true" />
+                    oglissane@gmail.com
+                  </ListItemContent>
+                </ListItem>
+              </List>
+            </div>
+          </Cell>
+        </Grid>
+      </div>
+    );
+  }
+}
+
+export default Contact;
